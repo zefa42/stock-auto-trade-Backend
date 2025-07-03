e08878db1769905acd6cd28e8d4b3a12e00725fb
zefa42 <youngoh07015@gmail.com>
2025-07-03 11:36:26 +0900

    Initial commit

---
+++ stock-auto-trade/src/main/java/com/zefa/stock_auto_trade/StockAutoTradeApplication.java
@@ -0,0 +1,13 @@
+package com.zefa.stock_auto_trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAutoTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAutoTradeApplication.class, args);
+	}
+
+}
---
+++ stock-auto-trade/src/test/java/com/zefa/stock_auto_trade/StockAutoTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zefa.stock_auto_trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAutoTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
