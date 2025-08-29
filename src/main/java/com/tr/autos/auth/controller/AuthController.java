3e11804a058888101c7f9a00d4abc75e194a07c4
zefa42 <youngoh07015@gmail.com>
2025-08-30 01:10:04 +0900

    feat: 로그인부 Controller 구현

--- src/main/java/com/tr/autos/auth/controller/AuthController.java
+++ src/main/java/com/tr/autos/auth/controller/AuthController.java
@@ -1,16 +1,15 @@
 package com.tr.autos.auth.controller;
 
 import com.tr.autos.auth.dto.request.SignupRequestDto;
+import com.tr.autos.auth.dto.request.LoginRequestDto;
+import com.tr.autos.auth.dto.response.LoginResponseDto;
 import com.tr.autos.auth.dto.response.SignupResponseDto;
 import com.tr.autos.auth.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -22,5 +21,11 @@ public class AuthController {
     public ResponseEntity<SignupResponseDto> signup(@RequestBody @Valid SignupRequestDto signupRequestDto) {
         SignupResponseDto res = authService.signup(signupRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody @Valid LoginRequestDto loginRequestDto) {
+        LoginResponseDto res = authService.login(loginRequestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 }
